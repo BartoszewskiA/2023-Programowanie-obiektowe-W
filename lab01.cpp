cd8f18d709c44504647cfeeb9132584c0d2bc72a
BartoszewskiA <bartoszewski.a@outlook.com>
2023-04-14 11:18:58 +0200

    14.04.2023

--- lab01.cpp
+++ lab01.cpp
@@ -10,11 +10,7 @@ protected:
     double y;
 
 public:
-    Punkt()
-    {
-        x = 0;
-        y = 0;
-    }
+    Punkt() : x(0), y(0) {}
     Punkt(double X, double Y) : x(X), y(Y) {}
     void setX(double x) { this->x = x; }
     void setY(double y) { this->y = y; }
@@ -42,6 +38,8 @@ protected:
     double b;
 
 public:
+    Prostokat() : Punkt(), a(0), b(0) {}
+    Prostokat(double X, double Y, double A, double B) : Punkt(X, Y), a(A), b(B) {}
     void setA(double a) { this->a = a; }
     void setB(double b) { this->b = b; }
     double getA() { return a; }
@@ -55,6 +53,27 @@ public:
         stringstream s;
         s << Punkt::info() << " a=" << a << " b=" << b << " pole=" << getPole();
         return s.str();
+    }
+};
+//---------------------------------------------------------------------------------------//
+
+class Prostopadloscian : public Prostokat
+{
+protected:
+    double z;
+    double h;
+
+public:
+    Prostopadloscian() : Prostokat(), z(0), h(0) {}
+    Prostopadloscian(double X, double Y, double Z, double A, double B, double H)
+        : Prostokat(X, Y, A, B), z(Z), h(H) {}
+    double getPole()
+    {
+        return 2 * Prostokat::getPole() + 2 * a * h + 2 * b * h;
+    }
+    double getObjetosc()
+    {
+        return Prostokat::getPole() * h;
     }
 };
 
